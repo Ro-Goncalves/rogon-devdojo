3771c849fb48e719aa9a838f1205cceb574a6a21
Rodrigo Gonçalves <ro.go.calves@gmail.com>
2022-08-04 13:16:30 -0300

    Bloco finally

--- curso-java-completo/src/rogon/javacompleto/trowable/exceptions/checked/CheckedExceptionsTest.java
+++ curso-java-completo/src/rogon/javacompleto/trowable/exceptions/checked/CheckedExceptionsTest.java
@@ -5,13 +5,16 @@ import java.io.IOException;
 
 public class CheckedExceptionsTest {
 	public static void main(String[] args) {
-		try {
-			criarArquivo();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+//		try {
+//			criarArquivo();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+		
+		abrirArquivo();
 	}
 	
+	//Lança a exceção, quem chamar o método que deve tratá=lo
 	public static void criarArquivo() throws IOException {
 		
 		File file = new File("texte.txt");		
@@ -20,12 +23,35 @@ public class CheckedExceptionsTest {
 		
 	}
 	
+	//finally sempre será executado, ele executa mesmo quando existe retorno
+	public static String abrirArquivo() {
+		
+		try {
+			System.out.println("Abrindo um arquivos");
+			System.out.println("Executando a leitura do arquivos");
+			//throw new Exception();
+			System.out.println("Escrevendo no arquivo");	
+			return "Valor";
+		} catch(Exception e) {
+			System.out.println("Dentro do catch");
+			e.printStackTrace();			
+		} finally {
+			System.out.println("Fechado o arquivo");
+		}
+		
+		return null;
+		
+		
+	}
+	
+	//Podemo tratar a exceção e lança-la novamente, ou outra
 	public static void criarArquivo2() throws IOException{
 		
 		try {
 			criarArquivo();
 		} catch (Exception e) {
-			// TODO: handle exception
+			e.printStackTrace();
+			throw e;
 		}
 		
 	}
